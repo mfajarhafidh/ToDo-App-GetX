e4c716c31958f3efa74ed6a769aaed3ad1a232a6
Mohamad Fajar Hafidh <mfajarhafidh9@gmail.com>
2023-03-10 14:15:48 +0700

    add part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:todo_app/app/data/services/storage/services.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async{
+  await GetStorage.init();
+  await Get.putAsync(()=>StorageService().init());
   runApp(
     GetMaterialApp(
       title: "Application",
