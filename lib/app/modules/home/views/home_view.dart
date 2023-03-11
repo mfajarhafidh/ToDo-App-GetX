b102d019825d25a658479e02fab742e6b75421e6
Mohamad Fajar Hafidh <mfajarhafidh9@gmail.com>
2023-03-11 15:35:36 +0700

    add part 2

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:todo_app/app/core/utils/extensions.dart';
+import 'package:todo_app/app/data/models/task.dart';
+import 'package:todo_app/app/modules/home/widgets/add_card.dart';
+import 'package:todo_app/app/modules/home/widgets/task_card.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -8,16 +12,29 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: SafeArea(
+          child: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(4.0.wp),
+            child: Text(
+              "My List",
+              style: TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Obx(() => GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                children: [
+                  ...controller.tasks
+                      .map((element) => TaskCard(task: element))
+                      .toList(),
+                  AddCard()
+                ],
+              ))
+        ],
+      )),
     );
   }
 }
